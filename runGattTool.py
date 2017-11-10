0a3911670bcc53436668ac9e4ccd226054bd5e41
lpete <lpete@umich.edu>
2017-11-09 19:31:16 -0500

    Adding code to connect to device with yellow tape

---
+++ runGattTool.py
@@ -0,0 +1,44 @@
+# Using Hexiwear with Python
+import pexpect
+import time
+ 
+DEVICE = "D9:04:7D:17:F7:80"
+ 
+print("Hexiwear address:"),
+print(DEVICE)
+ 
+# Run gatttool interactively.
+print("Run gatttool...")
+child = pexpect.spawn("sudo gatttool -i hci0 -t random -b D9:04:7D:17:F7:80 -I")
+ 
+# Connect to the device.
+print("Connecting to "),
+print(DEVICE),
+child.sendline("connect")
+child.expect("Connection successful", timeout=5)
+print(" Connected!")
+ 
+# function to transform hex string like "0a cd" into signed integer
+#def hexStrToInt(hexstr):
+#	val = int(hexstr[0:2],16) + (int(hexstr[3:5],16)<<8)
+#	if ((val&0x8000)==0x8000): # treat signed 16bits
+#	val = -((val^0xffff)+1)
+#	return val
+ 
+#while True:
+# Accelerometer
+child.sendline("char-write-req 0x0011 0100 -listen")
+while True:
+    child.expect("Notification handle = 0x0010 value: ", timeout=10)
+    child.expect("\r\n", timeout=10)
+    print("Accel: "),
+    print(child.before),
+    print(child.before[0:5])
+
+#child.sendline("char-read-hnd 0x30")
+
+#child.sendline("char-read-hnd 0x0011")
+#child.expect("Characteristic value/descriptor: ", timeout=10)
+#child.expect("\r\n", timeout=10)
+#print("Accel: "),
+#print(child.before)
