8f764cc2e8ac7359dad5bcb6a70a1b0a7e0f9a2c
siegelty <siegelty@umich.edu>
2017-11-20 20:54:51 -0500

    Added better state handling with changed testing structure

--- packageTest.py
+++ packageTest.py
@@ -1,55 +1,46 @@
-from unpackager import Unpackager
+from unpackager import Unpackager, States
 import unittest
 
-class TestUnpackager(unittest.TestCase):
+class TestIngest(unittest.TestCase):
 
     def test_correct_checksum(self):
         testString = "f00412345678BA3C"
         unpack = Unpackager()
-        self.assertEqual(unpack.ingest(testString), 2)
+        self.assertEqual(unpack.ingest(testString), States.PACKEGE_COMPLETE)
 
     def test_incorrect_checksum(self):
         testString = "f00412345678BA3D"
         unpack = Unpackager()
-        self.assertEqual(unpack.ingest(testString), 0)
+        self.assertEqual(unpack.ingest(testString), States.PACKAGE_FAILED)
 
     #TODO: Write test for multiple strings
     def test_broken_up_over_data_messages(self):
         testString1 = "f0041234"
         testString2 = "5678BA3C"
         unpack = Unpackager()
-        self.assertEqual(unpack.ingest(testString1), 1)
-        self.assertEqual(unpack.ingest(testString2), 2)
+        self.assertEqual(unpack.ingest(testString1), States.WAITING_FOR_END)
+        self.assertEqual(unpack.ingest(testString2), States.PACKEGE_COMPLETE)
     
     def test_broken_up_over_size(self):
         testString1 = "f0"
         testString2 = "0412345678BA3C"
         unpack = Unpackager()
-        self.assertEqual(unpack.ingest(testString1), 1)
-        self.assertEqual(unpack.ingest(testString2), 2)
+        self.assertEqual(unpack.ingest(testString1), States.WAITING_FOR_SIZE)
+        self.assertEqual(unpack.ingest(testString2), States.PACKEGE_COMPLETE)
 
     def test_broken_up_over_checksum1(self):
         testString1 = "f00412345678"
         testString2 = "BA3C"
         unpack = Unpackager()
-        self.assertEqual(unpack.ingest(testString1), 1)
-        self.assertEqual(unpack.ingest(testString2), 2)
+        self.assertEqual(unpack.ingest(testString1), States.WAITING_FOR_CHECKSUMBYTE1)
+        self.assertEqual(unpack.ingest(testString2), States.PACKEGE_COMPLETE)
 
     def test_broken_up_over_checksum2(self):
         testString1 = "f00412345678BA"
         testString2 = "3C"
         unpack = Unpackager()
-        self.assertEqual(unpack.ingest(testString1), 1)
-        self.assertEqual(unpack.ingest(testString2), 2)
-        
-testStrings = [
-    # "f000", #no size
-    # "f00100", #size 1
-    # "f034", #size52
-    # "94f0029811", #not initial packet
-    "f00412345678BA3C", #correct checksum
-    "f00412345678BA3D", #incorrect checksum
-]
+        self.assertEqual(unpack.ingest(testString1), States.WAITING_FOR_CHECKSUMBYTE2)
+        self.assertEqual(unpack.ingest(testString2), States.PACKEGE_COMPLETE)
 
 if __name__ == '__main__':
     unittest.main()
--- unpackager.py
+++ unpackager.py
@@ -8,6 +8,8 @@ class States:
     WAITING_FOR_END = 2
     WAITING_FOR_CHECKSUMBYTE1 = 3
     WAITING_FOR_CHECKSUMBYTE2 = 4
+    PACKEGE_COMPLETE = 5
+    PACKAGE_FAILED = -1
 
 class Unpackager:
     START_BYTE = '\xf0'
@@ -23,7 +25,12 @@ class Unpackager:
     
     def unpackage(self, message):
         # Ingest with context
-        return self.ingest(message)
+        if (self.ingest(message) == PACKAGE_FAILED):
+            print "Discarding packet for whatever reason, SALT THE EARTH"
+        elif(self.ingest(message) < PACKAGE_COMPLETE):
+            print "Still waiting for EOP"
+        elif(self.ingest(message) == PACKAGE_COMPLETE):
+            print "Package was completed, please handle"
         # React to shit
         pass
     
@@ -31,65 +38,54 @@ class Unpackager:
     def ingest(self, message):
         bitMessage = BitArray(hex=message)
         for byte in bitMessage.bytes:
-            print("In state ", self.state)
             byteHex = '0x' + byte.encode("hex")
             if (self.state == States.WAITING_FOR_START):
                 if byte == Unpackager.START_BYTE:
                     # loop through bits and check for start
-                    print("Found start byte")
                     self.buffer.append(byteHex)
                     self.state = States.WAITING_FOR_SIZE
                 else:
-                    print("found byte, but is not start so discarding")
+                    print("found byte, but is not start so discarding")                    
 
             elif (self.state == States.WAITING_FOR_SIZE):
-                print("Byte interpreted as size of ", ord(byte))
                 self.buffer.append(byteHex)
                 self.size = ord(byte)
                 self.packetsIngested = 0
                 self.state = States.WAITING_FOR_END
 
             elif (self.state == States.WAITING_FOR_END):
-                print("Ingesting new packet for things")
                 self.buffer.append(byteHex)
                 self.data.append(byteHex)
                 self.packetsIngested += 1
+                self.state
             elif (self.state == States.WAITING_FOR_CHECKSUMBYTE1):
-                print("Handling MSB of checksum")
                 self.checksum = ord(byte) << 8
                 self.buffer.append(byteHex)
                 self.state = States.WAITING_FOR_CHECKSUMBYTE2
             elif (self.state == States.WAITING_FOR_CHECKSUMBYTE2):
-                print("Handling LSB of checksum")
                 self.checksum += ord(byte)
                 self.buffer.append(byteHex)
-                return self.handleFullPacket()
+                self.state = self.handleFullPacket()
             else:
                 print ("improper or unhandled state of: ", self.state)
             
             # check if we've reached the "end" of a packet
-            if (self.packetDone()):
-                print("Packet was completed")
+            if (self.allDataRecieved()):
                 self.state = States.WAITING_FOR_CHECKSUMBYTE1
         # if we are here, return 1 for not done yet
-        return 1
+        return self.state
 
-    def packetDone(self):
+    def allDataRecieved(self):
         return self.packetsIngested == self.size and self.state == States.WAITING_FOR_END
 
     def handleFullPacket(self):
+        #TODO: Call actual typing and handling here
         if(self.calculateCRC()):
-            print("Checksum passed")
-            return 2
+            return States.PACKEGE_COMPLETE
         else :
-            print ("Checksum failed")
-            return 0
+            return States.PACKAGE_FAILED
 
     def calculateCRC(self):
-        # print("Data is: ")
-        # for byte in self.data.bytes:
-        #     print(byte.encode("hex")),
-        # print("Checksum is:"),
         return self.crcfunc(self.data.bytes) == self.checksum
 
 
