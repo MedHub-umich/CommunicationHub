e2f8849191eb54edd78ea0dab21c1ee1898c5f25
lpete <lpete@umich.edu>
2017-11-20 19:44:51 -0500

    Easier Testing main

---
+++ mainNoButton.py
@@ -0,0 +1,9 @@
+from connect import Devices, readFrom
+
+devices = Devices()
+
+connectedDevs = devices.connectDevices()
+while(len(connectedDevs) == 0):
+	connectedDevs = devices.connectDevices()
+
+readFrom(connectedDevs[0])
