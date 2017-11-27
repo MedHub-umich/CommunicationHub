fe0511ccf1d24a4cb2f646cda9c450055a53cc6d
Kishore-B-Rao <kishbrao@umich.edu>
2017-11-27 16:20:29 -0500

    Tyler sucks

---
+++ mainNoButton.py
@@ -0,0 +1,11 @@
+from connect import Devices, readFrom
+
+
+
+devices = Devices()
+
+connectedDevs = devices.connectDevices()
+while(len(connectedDevs) == 0):
+	connectedDevs = devices.connectDevices()
+
+readFrom(connectedDevs[0])
