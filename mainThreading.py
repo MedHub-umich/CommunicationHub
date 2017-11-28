3b9bc2371a03d2a7ab145c3e9d6db272e21d816c
lpete <lpete@umich.edu>
2017-11-28 08:29:29 -0500

    testing threading

--- mainThreading.py
+++ mainThreading.py
@@ -19,25 +19,34 @@ def main():
 	connectedDevs = BLEdevs.connectDevices()
 	while(len(connectedDevs) == 0):
 		connectedDevs = BLEdevs.connectDevices()
+
+	threads.append(threading.Thread((target = readFromThread, args = connectedDevs[0]))
+	threads[i].setDaemon(True)
+	threads[i].start() 
+
+	threads.append(threading.Thread(target = writeToThread, args = device))
+	threads[i].setDaemon(True)
+	threads[i].start()
+
 	
-	i = 0
-	for i, device in connectedDevs:  # may have bug off by one
-		threads.append(threading.Thread(target = readFromThread, args = device)) #possible bug
-		threads[i].setDaemon(True)
-		threads[i].start()
+	# i = 0
+	# for i, device in connectedDevs:  # may have bug off by one
+	# 	threads.append(threading.Thread(target = readFromThread, args = device)) #possible bug
+	# 	threads[i].setDaemon(True)
+	# 	threads[i].start()
 
-	#possibly have to manually reset i?
+	# #possibly have to manually reset i?
 	
-	for i,device in connectedDevs:
-		threads.append(threading.Thread(target = writeToThread, args = device))
-		threads[i].setDaemon(True)
-		threads[i].start()
+	# for i,device in connectedDevs:
+	# 	threads.append(threading.Thread(target = writeToThread, args = device))
+	# 	threads[i].setDaemon(True)
+	# 	threads[i].start()
 
-	alertValues = [00020000, 00020100, 00020101, 00020001]
+	# alertValues = [00020000, 00020100, 00020101, 00020001]
 
-	for j in alertValues:
-		alert = alertValues[j]
-		time.sleep(5)
+	# for j in alertValues:
+	# 	alert = alertValues[j]
+	# 	time.sleep(5)
 
 main()
 
