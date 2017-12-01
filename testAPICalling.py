9b2cb07100710ee30f4d817d2fc53a3a875f7f83
siegelty <siegelty@umich.edu>
2017-12-01 12:56:05 -0500

    Added basic request test

---
+++ testAPICalling.py
@@ -0,0 +1,26 @@
+import requests
+url='http://medhub-server.herokuapp.com/api/v1.0/add_data'
+data = '''{
+	"packets": [
+		{
+			"user": 2,
+			"type": 2,
+			"time": 100,
+			"data": [1, 2, 6, 8]
+		},
+		{
+			"user": 2,
+			"type": 3,
+			"time": 20,
+			"data": [1, 2, 6, 8]
+		},
+		{
+			"user": 2,
+			"type": 1,
+			"time": 1,
+			"data": [1, 30, 6, 8]
+		}
+	]
+}'''
+
+response = requests.post(url, data=data)
