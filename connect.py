667b77ce60f782fc5e7e4f996516fa555dbd6b52
lpete <lpete@umich.edu>
2017-12-05 10:33:43 -0500

    more debugging

--- connect.py
+++ connect.py
@@ -50,7 +50,7 @@ class DeviceContainer:
             print("device registered as connected")
             self.numDevices += 1
             self.connectedDevs.append(temp)
-            self.readThreads.append(threading.Thread(target=self.readFrom, args=(temp.index)))
+            self.readThreads.append(threading.Thread(target=readFromThread, args=(self, temp.index)))
             self.readThreads[temp.index].setDaemon(True)
             self.readThreads[temp.index].start()
 
@@ -111,10 +111,10 @@ class DeviceContainer:
                 pass
             else:
                 self.connectedDevs[index].devHandle.expect("\r\n")
-                self.connectedDevs[index].parser.unpackage(self.connectedDevs[index].devHandle.before)
+                self.connectedDevs[index].parser.unpackage(connectedDevs[index].devHandle.before)
 
-# def readFromThread(connectedDevs, index):
-    # connectedDevs.readFrom(index)
+def readFromThread(connectedDevs, index):
+    connectedDevs.readFrom(index)
 
 
 
