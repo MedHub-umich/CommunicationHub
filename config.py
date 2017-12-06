0e00b79efdab717614fbceb7e2de4eed6753849d
Kishore-B-Rao <kishbrao@umich.edu>
2017-12-05 23:20:22 -0500

    fjkdslj

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 MACtranslation = {
-	# "EC:B1:FE:A2:84:01" : 1,
+	"EC:B1:FE:A2:84:01" : 2,
 	# "D9:04:7D:17:F7:80" : 1,
 	# "EF:DD:9C:D6:FB:6B" : 1,
 	# "F3:C9:F9:A0:E9:6E" : 1,
@@ -9,7 +9,7 @@ MACtranslation = {
 
 userToMac = {
 	1 : "FA:9A:A3:54:EE:DA",
-	2 : "D9:04:7D:17:F7:80" # TODO: Make me not here
+	2 : "EC:B1:FE:A2:84:01"
 }
 
 class PacketTypes:
