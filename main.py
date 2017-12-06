319ae1d4596300078dad4b9d27993f3304fba7cc
lpete <lpete@umich.edu>
2017-12-06 04:09:11 +0000

    Lauren doesnt know what she is doing... really

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from connect import *
 import time
 
+# For button press
+import RPi.GPIO as GPIO
+
+# Pin Definitions:
+ledPin = 23 # Broadcom pin 23 (P1 pin 16)
+butPin = 17 # Broadcom pin 17 (P1 pin 11)
+connect = 0
+timeOut = 0
+ledState = False
+
+
 connectedDevs = connect()
 
 time.sleep(2)
