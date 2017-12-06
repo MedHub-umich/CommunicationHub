ba401dfba85d6188ddcaeb33b80bce95eea736cf
siegelty <siegelty@umich.edu>
2017-12-05 22:13:03 -0500

    Some noice debuggers

--- mainWrite.py
+++ mainWrite.py
@@ -20,13 +20,16 @@ class writer(Thread):
 				for dev in self.connectedDevs:
 					if dev.MACaddress == macAddr:
 						userDevice = dev
-				r = requests.get('http://medhub-server.herokuapp.com/api/v1.0/alert/' + str(user)).json()
-				print(userDevice)
+				r = requests.get('http://medhub-server.herokuapp.com/api/v1.0/alert/' + str(user))
+				r = r.json()
 				if len(r['alerts']) > 0 and not userDevice is None:
 					for alert in r['alerts']:
 						print ("Sending alert "),
 						print (alert['data'])
 						individualWrite.writeWorker(userDevice, alert['data'])
+				else:
+					print("No alert found for user: "),
+					print(user)
 			time.sleep(2)
 
 
