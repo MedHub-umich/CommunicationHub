5ce9721c011eec4668dc530987658ce4fdc34cbb
siegelty <siegelty@umich.edu>
2017-12-05 22:11:30 -0500

    Changed debugging prints

--- mainWrite.py
+++ mainWrite.py
@@ -20,8 +20,8 @@ class writer(Thread):
 				for dev in self.connectedDevs:
 					if dev.MACaddress == macAddr:
 						userDevice = dev
-				r = requests.get('http://medhub-server.herokuapp.com/api/v1.0/alert/' + str(user))
-				print(r.json()['alerts'])
+				r = requests.get('http://medhub-server.herokuapp.com/api/v1.0/alert/' + str(user)).json()
+				print(userDevice)
 				if len(r['alerts']) > 0 and not userDevice is None:
 					for alert in r['alerts']:
 						print ("Sending alert "),
