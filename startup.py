aeb8d424390981e19021d7548bdb649d09c75bb9
Kishore-B-Rao <kishbrao@umich.edu>
2017-12-06 21:31:34 -0500

    fjd

--- startup.py
+++ startup.py
@@ -1,11 +1,12 @@
 import RPi.GPIO as GPIO
-Import time
+import time
+import pexpect
 import subprocess
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIOIN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
     input_state = GPIO.input(4)
     if input_state == False:
-        subprocess.call("/home/pi/Documents/MedHub/CommunicationHub", shell=True)
+        pexpect.spawn("sudo python /home/pi/Documents/MedHub/CommunicationHub/main.py")
